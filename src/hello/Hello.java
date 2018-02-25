099484c6955f7b5679496276f4213e2835c0963a
Edek <rom334@wp.pl>
2018-02-25 10:10:36 +0100

    Druga osoba zostala utworzona

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,6 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Yo world!");
+		
+		Osoba druga = WczytajOsobe.nowaOsoba();
+		System.out.println("Druga osoba to:" + druga);
 
 	}
 
